96b8cce8a7886300f841088907642fb5ecaf29e4
Hamon Julien <hamon.julien7@gmail.com>
2025-06-01 18:05:41 +0200

    Ajout bestrating / createrating

--- routes/books.js
+++ routes/books.js
@@ -5,8 +5,10 @@ const booksCtrl = require ('../controllers/books')
 const auth = require ('../middleware/auth')
 
 router.get('/', booksCtrl.getAllBooks);
+router.get('/bestrating', booksCtrl.getBestRating);
 router.get('/:id', booksCtrl.getOneBook);
 router.post('/' , auth, upload, upload.resizeImage, booksCtrl.createBook);
+router.post('/:id/rating', auth, booksCtrl.createRating);
 router.put('/:id', auth, upload, upload.resizeImage, booksCtrl.modifyBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
